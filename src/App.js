26ed2bd74a07341f73d5cc0c6926990103ed5a85
Jonathan Langdon <jlan728@wgu.edu>
2024-10-09 14:18:58 -0400

    github pages routing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import NotFound from './components/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/michiganReactJSSite">
       <Navbar />
       <main>
         <Routes>
